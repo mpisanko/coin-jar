087ef39c5f604333c39a32ef1f5c79354229e001
Michal Pisanko <mpisanko@gmail.com>
2020-05-05 22:31:10 +1000

    seed database with Currencies which need to be checked

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CurrencyPair.create(currency_1: 'BTC', currency_2: 'AUD')
+CurrencyPair.create(currency_1: 'ETH', currency_2: 'AUD')
