5e821f9a63b7cd65565d9a0c5c4298d81e5dff77
Michal Pisanko <mpisanko@gmail.com>
2020-05-05 16:33:16 +1000

    specify price as immutable

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Price, type: :model do
   subject do
     described_class.new(
-      { last: 1000,
-        ask: 1001,
-        bid: 999,
-        price_at: Time.current,
-        currency_pair: CurrencyPair.new({ currency_1: 'ETH', currency_2: 'AUD' }) }
+      last: 10000.00666,
+      ask: 10001.00999,
+      bid: 9999.00333,
+      price_at: Time.current,
+      currency_pair: CurrencyPair.new(currency_1: 'ETH', currency_2: 'AUD')
     )
   end
 
@@ -20,5 +20,17 @@ RSpec.describe Price, type: :model do
     it "is not valid without #{attribute}" do
       expect(price).to_not be_valid
     end
+  end
+
+  %i[last bid ask price_at currency_pair].each do |attribute|
+    it "has readable #{attribute}" do
+      expect(subject.public_send(attribute)).not_to be_nil
+    end
+  end
+
+  it 'is immutable after creation' do
+    subject.save
+    subject.last += 666
+    expect { subject.save }.to raise_error(ActiveRecord::ReadOnlyRecord)
   end
 end
