e67395b1174fca6379f04181e70d85c3bebb15a1
Michal Pisanko <mpisanko@gmail.com>
2020-05-06 12:45:46 +1000

    fill in the specs and implement service

--- app/services/currency_price.rb
+++ app/services/currency_price.rb
@@ -2,12 +2,31 @@ require 'rest_client'
 
 # Currency Price Service - reads CoinJar price ticker
 class CurrencyPrice
+  TICKER = 'https://data.exchange.coinjar.com/products/%s/ticker'.freeze
 
-  def btc()
-
+  def btc
+    save_price(CurrencyPair.find_by(currency_1: 'BTC'))
   end
 
-  def eth()
+  def eth
+    save_price(CurrencyPair.find_by(currency_1: 'ETH'))
+  end
 
+  private
+
+  def request_price(currency_pair)
+    response = RestClient.get(TICKER % currency_pair.to_s)
+    ActiveSupport::JSON.decode(response.body)
+  end
+
+  def save_price(currency)
+    json_price = request_price(currency)
+    Price.new(
+      last:          json_price["last"].to_d,
+      bid:           json_price["bid"].to_d,
+      ask:           json_price["ask"].to_d,
+      price_at:      json_price["current_time"].to_time,
+      currency_pair: currency
+    ).tap(&:save)
   end
 end
--- spec/services/currency_price_spec.rb
+++ spec/services/currency_price_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'CurrencyPrice' do
-  subject { CurrencyPrice.new }
-  let(:currencies) { %w[ETHAUD BTCAUD] }
+RSpec.describe CurrencyPrice do
+  subject { described_class.new }
   let(:response) { RestClient::Response.new }
   describe 'btc' do
-    let(:btc) { CurrencyPair.new(currency_1: 'BTC', currency_2: 'AUD').tap(&:save) }
+    let!(:btc) { CurrencyPair.new(currency_1: 'BTC', currency_2: 'AUD').tap(&:save) }
     let(:btc_price) do
       {
         'last'         => '14090.00000000',
         'current_time' => '2020-05-06T00:12:33.345770Z',
         'bid'          => '14040.00000000',
-        'ask'          => '14080.00000000'
+        'ask'          => '14080.00000000',
       }.to_json
     end
     context 'request successful' do
@@ -20,30 +19,37 @@ RSpec.describe 'CurrencyPrice' do
         allow(response).to receive(:body).and_return(btc_price)
       end
       it 'saves the price it read' do
+        expect(RestClient).to receive(:get).with(CurrencyPrice::TICKER % 'BTCAUD')
         subject.btc
-        expect(Price.last.bid).to eq(14040.0)
+        saved = Price.last
+        expect(saved.bid).to eq(14040.0)
+        expect(saved.currency_pair.to_s).to eq('BTCAUD')
       end
     end
   end
 
   describe 'eth' do
-    let(:eth) { CurrencyPair.new(currency_1: 'ETH', currency_2: 'AUD').tap(&:save) }
+    let!(:eth) { CurrencyPair.new(currency_1: 'ETH', currency_2: 'AUD').tap(&:save) }
     let(:eth_price) do
       {
         'last'          => '318.20000000',
         'current_time'  => '2020-05-06T00:14:38.144159Z',
         'bid'           => '317.80000000',
-        'ask'           => '320.30000000'
+        'ask'           => '320.30000000',
       }.to_json
     end
     context 'request successful' do
       before do
         allow(RestClient).to receive(:get).and_return(response)
-        allow(response).to receive(:body).and_return(eth_price.to_json)
+        allow(response).to receive(:body).and_return(eth_price)
       end
       it 'saves the price it read' do
+        expect(RestClient).to receive(:get).with(CurrencyPrice::TICKER % 'ETHAUD')
         subject.eth
-        expect(Price.last.bid).to eq(317.8)
+        saved = Price.last
+        puts "saved #{saved}"
+        expect(saved.bid).to eq(317.8)
+        expect(saved.currency_pair.currency_1).to eq('ETH')
       end
     end
   end
